082eb667156b351aa3aaea1f6ac9f5f52e0aa4a1
meta-tanu-mittal <41181771+meta-tanu-mittal@users.noreply.github.com>
2018-08-21 11:32:59 +0530

    Update Session4-Q2.sql

--- DBMS-Session4/Database4/Session4-Q2.sql
+++ DBMS-Session4/Database4/Session4-Q2.sql
@@ -1,22 +1,24 @@
+
 #DBMS Session4 Q2
 USE store_front;
 
 /*Create a Stored procedure to retrieve average sales of each product in a month.
 Month and year will be input parameter to function.*/
-DROP PROCEDURE IF EXISTS get_average;
+DROP PROCEDURE IF EXISTS monthly_sales;
+
 DELIMITER $$
 
-CREATE PROCEDURE get_average(IN year INT,IN month INT)
+CREATE PROCEDURE monthly_sales(IN year INT,IN month INT)
 	BEGIN
-	SELECT p.name,COUNT(od.product_id)
+	SELECT p.name,SUM(od.product_quantity)
 	FROM product p
 	INNER JOIN order_details od ON p.id=od.product_id
 	INNER JOIN order_placed op ON od.order_id=op.id
-	WHERE YEAR(op.order_date)=year #AND MONTH(op.order_placed)=month
+	WHERE YEAR(op.order_date)=year AND MONTH(op.order_date)=month
 	GROUP BY od.product_id;
  END $$
  
- CALL get_average(2018,5);
+ CALL monthly_sales(2018,2);
 
 
 /*Create a stored procedure to retrieve table having order detail with status for a given period. 
@@ -24,12 +26,13 @@ Start date and end date will be input parameter.
 Put validation on input dates like start date is less than end date. 
 If start date is greater than end date take first date of month as start date.
 */
+DROP PROCEDURE IF EXISTS find_order_details;
 DELIMITER $
 CREATE PROCEDURE find_order_details(IN start_date DATE,IN end_date DATE)
 BEGIN
 	IF start_date > end_date
 	THEN
-		SET start_date=end_date-INTERVAL DAY(end_date);
+		SET start_date=end_date-(DAY(end_date)-1);
 	END IF;
 
 	SELECT p.name,od.status
@@ -40,3 +43,4 @@ BEGIN
 
 END $
 
+CALL find_order_Details('2018-04-01','2018-06-01');
