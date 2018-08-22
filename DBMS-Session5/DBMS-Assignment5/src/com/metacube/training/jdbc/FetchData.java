0832a151db6395a261d2fcce71e6c1d3ca686374
meta-tanu-mittal <41181771+meta-tanu-mittal@users.noreply.github.com>
2018-08-22 14:31:25 +0530

    Update FetchData.java

--- DBMS-Session5/DBMS-Assignment5/src/com/metacube/training/jdbc/FetchData.java
+++ DBMS-Session5/DBMS-Assignment5/src/com/metacube/training/jdbc/FetchData.java
@@ -29,15 +29,10 @@ public class FetchData {
 	public List getAllOrders(int user_id) {
 		List<OrderDetail> listOfOrders = new ArrayList<OrderDetail>();
 
-		String query = "SELECT  order_placed.id,order_placed.order_date,SUM(order_details.price*order_details.product_quantity)  AS price "
-				+ "FROM user " + "INNER JOIN order_placed ON user.id=order_placed.user_id "
-				+ "INNER JOIN order_details ON order_placed.id=order_details.order_id " + "WHERE user.id=?"
-				+ " AND order_details.status='shipped'" + "GROUP BY order_details.order_id";
-
 		Connection con = DBConnection.getConnection();
 
 		try {
-			PreparedStatement ps = con.prepareStatement(query);
+			PreparedStatement ps = con.prepareStatement(DBQueries.userOrder);
 			ps.setInt(1, user_id);
 
 			ResultSet res = ps.executeQuery();
@@ -46,14 +41,14 @@ public class FetchData {
 				Date date = res.getDate("order_date");
 				float price = res.getFloat("price");
 				listOfOrders.add(new OrderDetail(id, date, price));
-
+     
 				System.out.println(id + " " + date + " " + price);
 			}
+		
 		} catch (SQLException e) {
 
 			e.printStackTrace();
 		}
-
 		return listOfOrders;
 
 	}
@@ -67,10 +62,9 @@ public class FetchData {
 	 */
 	public int insertImageInBatch(List<Image> img) {
 		int[] result = null;
-		String strInsert = "insert into product_image(name,image_url,product_id) values(?,?,?)";
 		try (Connection con = DBConnection.getConnection();
 
-				PreparedStatement ps = con.prepareStatement(strInsert);) {
+				PreparedStatement ps = con.prepareStatement(DBQueries.insertImage);) {
 			try {
 				con.setAutoCommit(false);
 
@@ -102,15 +96,11 @@ public class FetchData {
 	 * @return count of inactive products
 	 */
 	public int changeProductStatus() {
-		String query = "UPDATE product " + "SET product.product_state='inactive' " + "WHERE product.id NOT IN "
-				+ "(SELECT order_details.product_id " + "FROM order_placed "
-				+ "INNER JOIN order_details ON order_placed.id=order_details.order_id "
-				+ "WHERE DATEDIFF(CURDATE(),order_placed.order_date)<365)";
 		
 		int res = 0;
 		Connection con = DBConnection.getConnection();
 		try {
-			PreparedStatement ps = con.prepareStatement(query);
+			PreparedStatement ps = con.prepareStatement(DBQueries.updateProductStatus);
 			res = ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -126,13 +116,10 @@ public class FetchData {
 	 */
 	public List findTopCategoryAndChild() {
 		List<ParentAndChild> listOfCategory = new ArrayList<ParentAndChild>();
-		String query = "SELECT parent.name, COUNT(*)  AS noOfChild " + "FROM category AS parent "
-				+ "INNER JOIN category AS child ON child.parent_id=parent.id " + "GROUP BY child.parent_id "
-				+ "ORDER BY parent.name";
 
 		Connection con = DBConnection.getConnection();
 		try {
-			PreparedStatement ps = con.prepareStatement(query);
+			PreparedStatement ps = con.prepareStatement(DBQueries.countChild);
 			ResultSet res = ps.executeQuery();
 			while (res.next()) {
 				String name = res.getString("name");
