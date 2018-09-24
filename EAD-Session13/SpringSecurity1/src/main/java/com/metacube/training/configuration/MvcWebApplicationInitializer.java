0999da811a8e59519ae84bbb084c7e2b944789ac
meta-tanu-mittal <tanu.mittal@metacube.com>
2018-09-24 23:39:09 +0530

    EAD-Session13

---
+++ EAD-Session13/SpringSecurity1/src/main/java/com/metacube/training/configuration/MvcWebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.metacube.training.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+   @Override
+   protected Class<?>[] getRootConfigClasses() {
+	   return new Class[] { SpringSecurity.class};
+   }
+
+   @Override
+   protected Class<?>[] getServletConfigClasses() {
+      return new Class[] { MvcWebConfig.class };
+   }
+
+   @Override
+   protected String[] getServletMappings() {
+      return new String[] { "/" };
+   }
+   
+
+}
